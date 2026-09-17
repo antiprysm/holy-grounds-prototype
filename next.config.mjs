e08b03345ac5a3023f05a14babfff5ac1a569958
antiprysm <59176422+antiprysm@users.noreply.github.com>
2026-09-16 19:49:38 -0500

    Configure GitHub Pages deployment

---
+++ next.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import("next").NextConfig} */
+const nextConfig = {
+  output: "export",
+  basePath: "/holy-grounds-prototype",
+  images: {
+    unoptimized: true,
+  },
+};
+
+export default nextConfig;
